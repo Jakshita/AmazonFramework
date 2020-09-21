fe48bb2be3c28acd4401276227470d87863b64dc
Jakshita <71448851+Jakshita@users.noreply.github.com>
2020-09-21 16:38:07 +0530

    Update SignOut.java

--- src/test/java/testCases/SignOut.java
+++ src/test/java/testCases/SignOut.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import pageObjects.*;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
@@ -16,17 +17,19 @@ public class SignOut extends Base {
 	public void signOut() throws IOException {
 		services = startServer();
 		AndroidDriver<AndroidElement> driver = Capabilities();//getting capabilities in the driver
+		Reporter.log("driver fatched");
 		SignOutObject signin = new SignOutObject(driver);
+		Reporter.log("In the sign out test");
 		Utility.getWaitDriver(driver).until(ExpectedConditions.presenceOfElementLocated(SignOutObject.homePage));//let the home page gets loaded
-		SignOutObject.getSideBar().click();
-		Utility.getScrollTo(driver, "Settings");
+		SignOutObject.getSideBar().click();//clicking on the hamunger menu
+		Utility.getScrollTo(driver, "Settings");//going for the settings
 		SignOutObject.getSettings().click();
 		try {
 			if(SignOutObject.getSignOutOption().isDisplayed()) {
 				SignOutObject.getSignOutOption().click();
 				SignOutObject.getConfirm().click();
 			}else {
-				System.out.println("already signout");
+				Reporter.log("Already sign-out");
 			}
 			
 		}catch(Exception e) {
