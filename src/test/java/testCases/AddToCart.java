ccc1cab1408c4606e88f6b8ddd791a7698807154
Akshita jain <71448851+Jakshita@users.noreply.github.com>
2020-09-28 15:53:16 +0530

    Update AddToCart.java

--- src/test/java/testCases/AddToCart.java
+++ src/test/java/testCases/AddToCart.java
@@ -8,12 +8,14 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -32,6 +34,12 @@ public class AddToCart extends Base {
 		public static String name;//declaring varibles globally so that can compare in another test
 		public static String size;
 		public static String price;
+		static Logger log;
+		@BeforeClass
+		public static void main(String []args) {
+			log = Logger.getLogger(AddToCart.class.getName()); //Logger variable gets initilize.
+		}
+		
 		@DataProvider
 		public Object[][] getProductSearch() throws InvalidFormatException, IOException{
 			return FatchExcelData.excelData(System.getProperty("user.dir")+"\\programData.xlsx","Sheet2");
@@ -40,14 +48,13 @@ public class AddToCart extends Base {
 		public void selectProduct(String s,String s1) throws IOException, InterruptedException {
 			services = startServer();
 			AndroidDriver<AndroidElement> driver=Capabilities();
-			Reporter.log("driver fatched");
 			AddToCartObject add =new AddToCartObject(driver);
-			Reporter.log("In the select product test");
+			log.info("In the select product test");
 			Utility.getWaitDriver(driver).until(ExpectedConditions.presenceOfElementLocated(AddToCartObject.homePage));//util homepage gets loaded
 			AddToCartObject.getSearchOption().click();//going for the search option
 			driver.getKeyboard().sendKeys(" "+s);
 			driver.getKeyboard().sendKeys(Keys.ENTER);
-			Reporter.log("product is searching");
+			log.info("product is searching");
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			Utility.getScrollTo(driver, s1);
 			Utility.getWaitDriver(driver).until(ExpectedConditions.presenceOfElementLocated(AddToCartObject.searchPage));//wait until scroll to certain object
@@ -56,14 +63,14 @@ public class AddToCart extends Base {
 		}
 		@Test(dependsOnMethods={"selectProduct"} )
 		public void productDetail() throws IOException {
-			Reporter.log("In the product detail test");
+			log.info("In the product detail test");
 			AddToCartObject add =new AddToCartObject(driver);
 			name=AddToCartObject.getName().getText();
-			Reporter.log("name taken");
+			log.info("name taken");
 			size=AddToCartObject.getSize().getText();
-			Reporter.log("size taken");
+			log.info("size taken");
 			price=AddToCartObject.getPrice().getText();
-			Reporter.log("price taken");
+			log.info("price taken");
 		}
 		@DataProvider
 		public Object[][] getAddToCartText() throws InvalidFormatException, IOException{
@@ -71,11 +78,11 @@ public class AddToCart extends Base {
 		}
 		@Test(priority=3,dataProvider="getAddToCartText")
 		public void addToCart(String s) throws IOException {
-				Reporter.log("In the add to cart test");
+				log.info("In the add to cart test");
 				Utility.getScrollTo(driver, s);
 				AddToCartObject add =new AddToCartObject(driver);
 				AddToCartObject.getAddToCartOption().click();
-				Reporter.log("product added to the cart");
+				log.info("product added to the cart");
 				AddToCartObject.getDone().click();
 		}
 		
@@ -87,15 +94,15 @@ public class AddToCart extends Base {
 			ProducCheckObject.getProduct().click();
 			try {
 				Assert.assertEquals(name, ProducCheckObject.getName().getText());
-				Reporter.log("name check");
+				log.info("name check");
 				Assert.assertEquals(price, ProducCheckObject.getPrice().getText());
-				Reporter.log("price check");
+				log.info("price check");
 				Assert.assertEquals(size, ProducCheckObject.getSize().getText());
-				Reporter.log("size check");
+				log.info("size check");
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-			Reporter.log("Yeah! TestRun Successfully");
+			log.info("Yeah! TestRun Successfully");
 			services.stop();
 		}
 }
